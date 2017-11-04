4dbfd93b600f0974bf314bb0d80cd6be99adb40f
AnTTSE61258 <trantuanan@Trans-MacBook-Pro.local>
2017-11-04 12:57:23 +0900

    Download from youtube api

--- app/controllers/recommend_song_controller.rb
+++ app/controllers/recommend_song_controller.rb
@@ -2,6 +2,10 @@ class RecommendSongController < ApplicationController
   protect_from_forgery with: :null_session
 
   def create_song
+    youtube_id = params["id"]
+    return render :json => "{error: id empty}", :status => 400 unless youtube_id
+    YoutubeDL.download "https://www.youtube.com/watch?v=#{youtube_id}", output: "/root/mp3tank_raw/#{youtube_id}"
+    debugger
     render :json => "{}"
   end
 end
