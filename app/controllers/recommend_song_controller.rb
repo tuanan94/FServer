fdb9d76cbf6b7f7b842e603729e616f1ac33bc69
AnTTSE61258 <trantuanan@Trans-MacBook-Pro.local>
2017-11-04 15:51:49 +0900

    Fix download step

--- app/controllers/recommend_song_controller.rb
+++ app/controllers/recommend_song_controller.rb
@@ -1,10 +1,20 @@
+require 'fileutils'
+
 class RecommendSongController < ApplicationController
   protect_from_forgery with: :null_session
-
+  OUTPUT_TMP_PATH = "/root/mp3tank_raw_tmp/"
+  OUTPUT_PATH = "/root/mp3tank_raw/"
   def create_song
     youtube_id = params["id"]
     return render :json => "{error: id empty}", :status => 400 unless youtube_id
-    YoutubeDL.download "https://www.youtube.com/watch?v=#{youtube_id}", output: "/root/mp3tank_raw/#{youtube_id}"
+    YoutubeDL.download "https://www.youtube.com/watch?v=#{youtube_id}", output: OUTPUT_TMP_PATH + "#{youtube_id}"
+    files = Dir[OUTPUT_TMP_PATH+"#{youtube_id}*"]
+    if (files.size != 1)
+      return render :json => "{error: many outputs}", :status => 400
+    end
+    target_file = files[0]
+    FileUtils.mv target_file, OUTPUT_PATH + target_file.split("/").last
+
     render :json => "{}"
   end
 end
