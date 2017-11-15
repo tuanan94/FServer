4091de2550577ba7de36dc0b915b9583b8a6b632
TuanAn <tuanan94@hotmail.com>
2017-11-16 00:03:58 +0900

    Fix rubocop

--- app/controllers/recommend_song_controller.rb
+++ app/controllers/recommend_song_controller.rb
@@ -4,24 +4,22 @@ class RecommendSongController < ApplicationController
   protect_from_forgery with: :null_session
   IS_DEBUGGING = true
   if
-    OUTPUT_TMP_PATH = "mp3tank_raw_tmp/"
-    OUTPUT_PATH = "mp3tank_raw/"
+    OUTPUT_TMP_PATH = 'mp3tank_raw_tmp/'.freeze
+    OUTPUT_PATH = 'mp3tank_raw/'.freeze
   else
-    OUTPUT_TMP_PATH = "/root/mp3tank_raw_tmp/"
-    OUTPUT_PATH = "/root/mp3tank_raw/"
+    OUTPUT_TMP_PATH = '/root/mp3tank_raw_tmp/'.freeze
+    OUTPUT_PATH = '/root/mp3tank_raw/'.freeze
   end
 
   def create_song
-    youtube_id = params["id"]
-    return render :json => "{error: id empty}", :status => 400 unless youtube_id
-    YoutubeDL.download "https://www.youtube.com/watch?v=#{youtube_id}", output: OUTPUT_TMP_PATH + "#{youtube_id}"
-    files = Dir[OUTPUT_TMP_PATH+"#{youtube_id}*"]
-    if (files.size != 1)
-      return render :json => "{error: many outputs}", :status => 400
-    end
+    youtube_id = params['id']
+    return render json: '{error: id empty}', status: 400 unless youtube_id
+    YoutubeDL.download "https://www.youtube.com/watch?v=#{youtube_id}", output: OUTPUT_TMP_PATH + youtube_id.to_s
+    files = Dir[OUTPUT_TMP_PATH + "#{youtube_id}*"]
+    return render json: '{error: many outputs}', status: 400 if files.size != 1
     target_file = files[0]
-    FileUtils.mv target_file, OUTPUT_PATH + target_file.split("/").last
+    FileUtils.mv target_file, OUTPUT_PATH + target_file.split('/').last
 
-    render :json => "{}"
+    render json: '{}'
   end
 end
