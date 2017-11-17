5ff9191f29d7634cfb41f294a605f8b2040e0153
TuanAn <tuanan94@hotmail.com>
2017-11-17 21:06:52 +0900

    add external page

---
+++ app/controllers/extend_controller.rb
@@ -0,0 +1,9 @@
+class ExtendController < ApplicationController
+  def request_waiting
+
+  end
+
+  def app_name
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post 'create_song' => 'recommend_song#create_song'
+  get 'extend/request_waiting'
+  get 'extend/app_name'
+
   mount Fserverapi::API => '/'
 end
